807208c2488f954dbed04d4b62daf232f17b7660
Reagandennis <dennisowiti215@gmail.com>
2023-08-19 21:49:38 +0300

    file = opencalculator/calc.java

---
+++ calculator/calc.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String[] args) {    
+        System.out.println("Hello, world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int b = sc.nextInt();
+        System.out.println("Enter the operation: ");
+        char op = sc.next().charAt(0);
+        int result = 0;
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+        }
+        System.out.println("The result is: " + result);
+    }
+    
+}
