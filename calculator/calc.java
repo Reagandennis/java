d95b8aa8aeb294ec02f0f507a220c052af9d79d9
Reagandennis <dennisowiti215@gmail.com>
2023-08-19 21:54:46 +0300

    new file:   numberguessgame/numb.java

--- calculator/calc.java
+++ calculator/calc.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class calc {
     public static void main(String[] args) {    
         System.out.println("Hello, world!");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number: ");
-        int a = sc.nextInt();
-        System.out.println("Enter the second number: ");
-        int b = sc.nextInt();
-        System.out.println("Enter the operation: ");
-        char op = sc.next().charAt(0);
-        int result = 0;
-        switch (op) {
-            case '+':
-                result = a + b;
-                break;
-            case '-':
-                result = a - b;
-                break;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the first number: ");
+            int a = sc.nextInt();
+            System.out.println("Enter the second number: ");
+            int b = sc.nextInt();
+            System.out.println("Enter the operation: ");
+            char op = sc.next().charAt(0);
+      
+            int result = 0;
+            switch (op) {
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+            }
+            
+            System.out.println("The result is: " + result);
         }
-        System.out.println("The result is: " + result);
     }
     
 }
---
+++ numberguessgame/numb.java
@@ -0,0 +1,7 @@
+package numberguessgame;
+
+public class numb {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
